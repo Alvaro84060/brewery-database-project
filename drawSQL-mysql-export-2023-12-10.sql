6ca4487cfa81b100f46111fb01a172f1ca6feb98
Alvaro Arraras <85067108+Alvaro84060@users.noreply.github.com>
2023-12-10 05:10:02 +0100

    Add files via upload
    I added a new table called "role_position", basically to divide role-specific data from employee-specific data. This allows me to add, modify, and remove roles without affecting individual employee records. Now having "employees", "employee_roles" and "role_position" also allows me to reduce redundancy and ensure that the information is stored in a structured and organized manner, helping to maintain data integrity and consistency.

---
+++ drawSQL-mysql-export-2023-12-10.sql
@@ -0,0 +1,103 @@
+CREATE TABLE `products`(
+    `product_id` INT UNSIGNED NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NULL,
+    `category` VARCHAR(255) NULL,
+    `description` TEXT NOT NULL,
+    `price` DECIMAL(8, 2) NULL
+);
+ALTER TABLE
+    `products` ADD PRIMARY KEY `products_product_id_primary`(`product_id`);
+CREATE TABLE `payments`(
+    `payment_id` INT UNSIGNED NULL AUTO_INCREMENT,
+    `order_id` INT NOT NULL,
+    `amount` DECIMAL(8, 2) NULL,
+    `payment_date` DATE NOT NULL,
+    `method_id` INT NOT NULL
+);
+ALTER TABLE
+    `payments` ADD PRIMARY KEY `payments_payment_id_primary`(`payment_id`);
+CREATE TABLE `orders`(
+    `order_id` INT UNSIGNED NULL AUTO_INCREMENT,
+    `employee_id` INT NOT NULL,
+    `customer_id` INT NOT NULL,
+    `order_date` DATE NULL,
+    `total_amount` DECIMAL(8, 2) NULL
+);
+ALTER TABLE
+    `orders` ADD PRIMARY KEY `orders_order_id_primary`(`order_id`);
+CREATE TABLE `employee_roles`(
+    `role_id` INT UNSIGNED NULL AUTO_INCREMENT,
+    `role_name` VARCHAR(255) NULL
+);
+ALTER TABLE
+    `employee_roles` ADD PRIMARY KEY `employee_roles_role_id_primary`(`role_id`);
+ALTER TABLE
+    `employee_roles` ADD UNIQUE `employee_roles_role_name_unique`(`role_name`);
+CREATE TABLE `order_details`(
+    `order_detail_id` INT UNSIGNED NULL AUTO_INCREMENT,
+    `order_id` INT NOT NULL,
+    `product_id` INT NOT NULL,
+    `quantity` INT NULL,
+    `amount` DECIMAL(8, 2) NULL
+);
+ALTER TABLE
+    `order_details` ADD PRIMARY KEY `order_details_order_detail_id_primary`(`order_detail_id`);
+CREATE TABLE `customers`(
+    `customer_id` INT UNSIGNED NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(255) NULL,
+    `last_name` VARCHAR(255) NULL,
+    `email` VARCHAR(255) NULL,
+    `phone_number` VARCHAR(255) NULL,
+    `registration_date` DATE NOT NULL
+);
+ALTER TABLE
+    `customers` ADD PRIMARY KEY `customers_customer_id_primary`(`customer_id`);
+ALTER TABLE
+    `customers` ADD UNIQUE `customers_email_unique`(`email`);
+CREATE TABLE `employees`(
+    `emp_id` INT UNSIGNED NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(255) NULL,
+    `last_name` VARCHAR(255) NULL,
+    `birth_date` DATE NOT NULL,
+    `phone_number` VARCHAR(255) NULL,
+    `email` BIGINT NOT NULL,
+    `hire_date` DATE NULL,
+    `role_id` BIGINT NOT NULL,
+    `salary` DECIMAL(8, 2) NOT NULL
+);
+ALTER TABLE
+    `employees` ADD PRIMARY KEY `employees_emp_id_primary`(`emp_id`);
+ALTER TABLE
+    `employees` ADD UNIQUE `employees_phone_number_unique`(`phone_number`);
+CREATE TABLE `payment_methods`(
+    `method_id` INT UNSIGNED NULL AUTO_INCREMENT,
+    `method_name` VARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    `payment_methods` ADD PRIMARY KEY `payment_methods_method_id_primary`(`method_id`);
+CREATE TABLE `role_position`(
+    `role_id` INT UNSIGNED NULL AUTO_INCREMENT,
+    `role_name` VARCHAR(255) NULL,
+    `role_description` VARCHAR(255) NOT NULL,
+    `salary` DECIMAL(8, 2) NULL
+);
+ALTER TABLE
+    `role_position` ADD PRIMARY KEY `role_position_role_id_primary`(`role_id`);
+ALTER TABLE
+    `role_position` ADD UNIQUE `role_position_role_name_unique`(`role_name`);
+ALTER TABLE
+    `employees` ADD CONSTRAINT `employees_role_id_foreign` FOREIGN KEY(`role_id`) REFERENCES `employee_roles`(`role_id`);
+ALTER TABLE
+    `orders` ADD CONSTRAINT `orders_customer_id_foreign` FOREIGN KEY(`customer_id`) REFERENCES `customers`(`customer_id`);
+ALTER TABLE
+    `orders` ADD CONSTRAINT `orders_employee_id_foreign` FOREIGN KEY(`employee_id`) REFERENCES `employees`(`emp_id`);
+ALTER TABLE
+    `payments` ADD CONSTRAINT `payments_method_id_foreign` FOREIGN KEY(`method_id`) REFERENCES `payment_methods`(`method_id`);
+ALTER TABLE
+    `order_details` ADD CONSTRAINT `order_details_order_id_foreign` FOREIGN KEY(`order_id`) REFERENCES `orders`(`order_id`);
+ALTER TABLE
+    `payments` ADD CONSTRAINT `payments_order_id_foreign` FOREIGN KEY(`order_id`) REFERENCES `orders`(`order_id`);
+ALTER TABLE
+    `role_position` ADD CONSTRAINT `role_position_role_id_foreign` FOREIGN KEY(`role_id`) REFERENCES `employee_roles`(`role_id`);
+ALTER TABLE
+    `order_details` ADD CONSTRAINT `order_details_product_id_foreign` FOREIGN KEY(`product_id`) REFERENCES `products`(`product_id`);
